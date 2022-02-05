a50c40d3dac228b2eb6c51b49b7775985de3ae0f
Nitikesh Vishal <nv2384@columbia.edu>
2022-02-05 13:33:35 +0530

    adding choice field

--- wpapp/models.py
+++ wpapp/models.py
@@ -22,12 +22,32 @@ class WorkpermitExample(models.Model):
         ('A','A'),
         ('B','B')
     )
-    activity = models.CharField(max_length= 255)
-    sub_activity = models.CharField(max_length = 255)
-    work_permit = models.CharField(max_length = 255)
-    location = models.CharField(max_length = 400, null = True, blank = True)
-    manager = models.CharField(max_length = 255)
-    assignee = models.CharField(max_length = 255)
+    SUBACTIVITY_CHOICE = (
+        ('X','X'),
+        ('Y','Y')
+    )
+    WORKPERMIT_CHOICE = (
+        ('Hot', 'Hot'),
+        ('Height', 'Height')
+    )
+    Location_Choice = (
+        ('TowerA', 'TowerA'),
+        ('TowerB', 'TowerB')
+    )
+    Manager_Choice = (
+        ('Bob','Bob'),
+        ('John', 'John')
+    )
+    Assignee_Choice = (
+        ('Ritu','Ritu'),
+        ('Riya','Riya')
+    )
+    activity = models.CharField(max_length= 255, choices= ACTIVITY_CHOICE)
+    sub_activity = models.CharField(max_length = 255, choices= SUBACTIVITY_CHOICE)
+    work_permit = models.CharField(max_length = 255, choices= WORKPERMIT_CHOICE)
+    location = models.CharField(max_length = 400, null = True, blank = True, choices= Location_Choice)
+    manager = models.CharField(max_length = 255, choices= Manager_Choice)
+    assignee = models.CharField(max_length = 255, choices= Assignee_Choice)
 
     def __str__(self):
         return self.activity
